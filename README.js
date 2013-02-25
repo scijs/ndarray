d08fe12741aa7920f3ca633dabb93dde1210e68d
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-25 11:42:56 -0600

    adding files for initial check in

---
+++ README.js
@@ -0,0 +1,4 @@
+ndarray
+=======
+Multidimensional arrays for JavaScript.
+
---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+function wrapArray(list, dtype) {
+}
+
+function dtype(array) {
+}
+
+function zeros(nshape, ndtype) {
+}
+
+function reshape(array, nshape, ndtype) {
+}
+
+function asArray(array) {
+}
+
+module.exports = wrapArray;
+module.exports.zeros = zeros;
+module.exports.dtype = dtype;
+module.exports.reshape = reshape;
+module.exports.asArray = asArray;
