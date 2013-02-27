05ce034a4ba4112b7a55490456691159a7daa76a
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-26 20:14:06 -0600

    removed a comment, need to add proper toString() support

--- test/test.js
+++ test/test.js
@@ -18,9 +18,20 @@ for(var i=0; i<y.shape[0]; ++i) {
   }
 }
 
-//console.log(y)
-
 x.hi(2,2).assign(y)
 x.lo(2,2).assign(y)
 
-console.log(x.hi(3,3));
+
+
+var x = ndarray.zeros([5,5], "float64", [0,1])
+var y = ndarray.zeros([5,5], "float64", [1,0])
+for(var i=0; i<x.shape[0]; ++i) {
+  for(var j=0; j<x.shape[1]; ++j) {
+    x.set(i,j, i + j * 10);
+  }
+}
+
+console.log(x)
+y.assign(x)
+console.log(y)
+
