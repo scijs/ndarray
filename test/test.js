7632e81a8a0f5ea2a096ae03fb67c40c9e2d7197
Mikola Lysenko <mikolalysenko@gmail.com>
2013-03-15 17:35:59 -0500

    adding more tests, more still needed though

--- test/test.js
+++ test/test.js
@@ -1,14 +1,39 @@
+var test = require("tap").test
 var ndarray = require("../index.js");
 
-var x = ndarray.zeros([5, 5])
-var y = x.hi(4,4).lo(1,1)
+test("ndarray", function(t) {
 
-for(var i=0; i<y.shape[0]; ++i) {
-  for(var j=0; j<y.shape[1]; ++j) {
-    y.set(i,j, 1)
-  }
-}
-console.log(y.toString())
-console.log(x.toString())
+  var p = ndarray(new Float32Array([1,2,3,4]), [2,2])
+  t.equals(p.shape.length, 2)
+  t.equals(p.shape[0], 2)
+  t.equals(p.shape[1], 2)
+  t.equals(p.stride[0], 2)
+  t.equals(p.stride[1], 1)
 
+  t.end()
+})
 
+test("zeros", function(t) {
+
+  var x = ndarray.zeros([5, 5])
+
+  t.equals(x.shape.length, 2)
+  t.equals(x.shape[0], 5)
+  t.equals(x.shape[1], 5)
+  
+  var y = ndarray.zeros([11,10,9], 'float32')
+  t.equals(ndarray.dtype(y), 'float32')
+  t.equals(y.shape.length, 3)
+
+  t.end()
+})
+
+test("accessor", function(t) {
+
+  t.end()
+})
+
+test("slicing", function(t) {
+
+  t.end()
+})
